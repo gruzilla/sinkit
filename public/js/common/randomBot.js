5b418eb00665c7e50e99e125f41652a81b50959e
Clemens MM <clemens@mautnermarkhof.com>
2018-05-05 14:37:52 +0200

    updated bot

--- public/js/common/randomBot.js
+++ public/js/common/randomBot.js
@@ -1,13 +1,24 @@
+
+var inUse = {
+    top: false,
+    bottom: false
+};
+
 var useAction = function(team,action) {
     switch (action) {
         case 1:
+            if(inUse[team]) break;
             completeScreen.game().updateBoat(team,{shield: 1});
+            inUse[team] = true;
+            setTimeout(function(team){
+                inUse[team] = false;
+            },4000);
             break;
         case 2:
-            completeScreen.game().updateBoat(team,{velocity: -25 * Math.random()*4});
+            completeScreen.game().updateBoat(team,{velocity: -25 * Math.random()*6});
             break;
         case 3:
-            completeScreen.game().updateBoat(team,{velocity: 25 * Math.random()*4});
+            completeScreen.game().updateBoat(team,{velocity: 25 * Math.random()*6});
             break;
         case 4:
             completeScreen.game().updateBoat(team,{fullstop: 1});
