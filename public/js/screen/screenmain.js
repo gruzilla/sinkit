a40cf779dc61a4f3fd6070dd43adace7916d610b
gruzilla <grillen@abendstille.at>
2018-05-05 13:30:07 +0200

    fix shield 2

--- public/js/screen/screenmain.js
+++ public/js/screen/screenmain.js
@@ -60,7 +60,7 @@ var Screen = function(messageDispatcher, gameState, airConsole){
             for (var i = 0; i < state.teamA.player.length; i++) {
                 var playerA = state.teamA.player[i];
                 if (typeof(playerA.actions) === 'undefined') {
-                    return;
+                    break;
                 }
                 if (!playerA.actions.hasOwnProperty('shield') || !playerA.actions.shield) {
                     teamAShield = false;
@@ -76,7 +76,7 @@ var Screen = function(messageDispatcher, gameState, airConsole){
             for (var j = 0; j < state.teamB.player.length; j++) {
                 var playerB = state.teamB.player[j];
                 if (typeof(playerB.actions) === 'undefined') {
-                    return;
+                    break;
                 }
                 if (!playerB.actions.hasOwnProperty('shield') || !playerB.actions.shield) {
                     teamBShield = false;
