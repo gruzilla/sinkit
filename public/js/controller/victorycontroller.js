057672c8828d3a5b3bd1ed5c3b2a84d47c59845d
gruzilla <grillen@abendstille.at>
2018-05-05 02:19:42 +0200

    victory controller

---
+++ public/js/controller/victorycontroller.js
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var TeamController = function(divId, messageDispatcher, airConsole) {
+
+    var onFinishedCallback = function() {};
+
+    /** game actions **/
+    // long running actions
+    function joinTeam(team) {
+        messageDispatcher.send(
+            'joinTeam',
+            { team: team }
+        );
+
+        return false;
+    }
+
+    function onFinished(callback) {
+        onFinishedCallback = callback;
+    }
+
+    function preload () {
+        //this.load.image('sky', 'src/games/firstgame/assets/sky.png');
+        //this.load.spritesheet('dude', 'src/games/firstgame/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    }
+
+    function create () {
+        airConsole.setOrientation(AirConsole.ORIENTATION_LANDSCAPE);
+
+        document.getElementById(divId).innerHTML = '' +
+            '<div id="header"><div id="title" class="v-center">Choose your team</div></div>' +
+            '<div id="teamA" class="button">Team Green</div>' +
+            '<div id="teamB" class="button">Team Red</div>';
+
+        // register event listener
+        document.getElementById('teamA').addEventListener('touchstart', function () { return joinTeam('A'); });
+        document.getElementById('teamB').addEventListener('touchstart', function () { return joinTeam('B'); });
+    }
+
+    messageDispatcher.register('playerUpdate', function(from, data) {
+        if (from !== AirConsole.SCREEN) {
+            return;
+        }
+
+        localStorage.setItem('playerData', JSON.stringify(data));
+        onFinishedCallback();
+    });
+
+    function update () {
+
+    }
+
+    return {
+        create: create,
+        onFinished: onFinished
+    };
+};
+
+
